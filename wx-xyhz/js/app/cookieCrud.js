79bff9994e528ae66a4f281591e1343d378a5098
qiuguixin <qguixin@qq.com>
2016-03-29 19:41:42 +0800

    test

---
+++ wx-xyhz/js/app/cookieCrud.js
@@ -0,0 +1,68 @@
+/**
+ * Created by guixin on 2016/3/29.
+ */
+//cookie操作
+require.config({
+    paths: {
+        'jquery': 'http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js'
+    }
+});
+define(['jquery'],
+    function ($) {
+        var
+            addCoo = function(key) {
+                var history,
+                    json = "[",
+                    json1,
+                    canAdd = true;
+                if (!$.cookie("history")) {
+                    history = $.cookie("history", "{name:\"" + key + "\"}", {
+                        expires: 1
+                    })
+                } else {
+                    history = $.cookie("history");
+                    console.log("222" + history);
+                    json1 = eval("(" + history + ")");
+                    console.log("json1:" + json1);
+                    $(json1).each(function() {
+                        if (this.name == key) {
+                            canAdd = false;
+                            return false
+                        }
+                    });
+                    if (canAdd) {
+                        $(json1).each(function() {
+                            json = json + "{\"name\":\"" + this.name + "\"},"
+                        });
+                        json = json + "{\"name\":\"" + key + "\"}]";
+                        $.cookie("history", json, {
+                            expires: 1
+                        })
+                    }
+                }
+            },
+
+            getCoo = function() {
+                if ($.cookie("history")) {
+                    var json = eval("(" + $.cookie("history") + ")"),
+                        list = [];
+                    $(json).each(function() {
+                        list.push(this.name)
+                    });
+                    return list
+                } else {
+                    return []
+                }
+            },
+
+            clearCoo = function() {
+                $.cookie("history", null)
+            }
+
+        return {
+            clearCoo: clearCoo, //清空cookie
+            addCoo: addCoo, //添加
+            getCoo: getCoo //获取
+        };
+    }
+);
--- wx-xyhz/js/comm/comm.js
+++ wx-xyhz/js/comm/comm.js
@@ -1,89 +1,9 @@
 /**
  * Created by xinyi on 2016/3/22.
  */
-//cookie操作
-var objCoo = {
-        clearCoo: clearCoo,//清空cookie
-        addCoo: addCoo,//添加
-        getCoo: getCoo//获取
-    };
-    console.log(objCoo.getCoo());
 
-    function addCoo(key) {
-        var history;
-        var json = "[";
-        var json1;
-        var canAdd = true;
-        if (!$.cookie("history")) {
-            history = $.cookie("history", "{name:\"" + key + "\"}", {
-                expires: 1
-            })
-        } else {
-            history = $.cookie("history");
-            console.log("222" + history);
-            json1 = eval("(" + history + ")");
-            console.log("json1:" + json1);
-            $(json1).each(function() {
-                if (this.name == key) {
-                    canAdd = false;
-                    return false
-                }
-            });
-            if (canAdd) {
-                $(json1).each(function() {
-                    json = json + "{\"name\":\"" + this.name + "\"},"
-                });
-                json = json + "{\"name\":\"" + key + "\"}]";
-                $.cookie("history", json, {
-                    expires: 1
-                })
-            }
-        }
-    }
 
-    function getCoo() {
-        if ($.cookie("history")) {
-            var json = eval("(" + $.cookie("history") + ")");
-            var list = [];
-            $(json).each(function() {
-                list.push(this.name)
-            });
-            return list
-        } else {
-            return []
-        }
-    }
 
-    function clearCoo() {
-        $.cookie("history", null)
-    }
-
-//返回顶部
-$(function() {
-    (function() { //返回顶部
-    var speed = 300;
-    var win_h = $(window).height(); //窗口高度  不用等所有内容加载完毕后就可获得
-    $(window).scroll(function(event) {
-        var st = $(window).scrollTop();
-        if (st > win_h) {
-            $('#to_top').stop().animate({
-                bottom: '66px',
-                opacity:1
-            }, speed);
-        } else {
-            $('#to_top').stop().animate({
-                bottom: 0,
-                opacity:0
-            }, speed);
-        }
-    });
-    $('#to_top').click(function(event) {
-        $('html,body').stop().animate({
-            scrollTop: 0
-        }, speed);
-    });
-})();
-});
 $(function(){
     //下载按钮点击事件
     if(getUrlParameter('show_invite')=='1'){
