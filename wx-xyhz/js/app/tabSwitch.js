3a40d808a6662efe15f4af3045c4719b0cc91940
shistriver <954917356@qq.com>
2016-04-01 18:03:57 +0800

    小 bug

--- wx-xyhz/js/app/tabSwitch.js
+++ wx-xyhz/js/app/tabSwitch.js
@@ -6,18 +6,8 @@ define(["jquery"],
         var
             tabBar = function() {
                 var check_img = ['url(img/ic_pay_checked.png)','url(img/ic_pay_check.png)']
-                /*$(className).on('click',function(){
-                    var check = $(className).is(':checked');
-                    if(check){
-                        $(this).css({'background-image':check_img[0]});
-                        $(this).attr("checked", true);
-                    }else{
-                        $(this).css({'background-image':check_img[1]});
-                        $(this).attr("checked", false);
-                    }
-                });*/
-                $('.item-list .pay_check_com').on('click',function(){
-                    $(this).css({'background-image':check_img[0]}).parent().siblings().find('.pay_check_com').css({'background-image':check_img[1]})
+                $('#payStyle .item-list').on('click',function(){
+                    $(this).find('.pay_check_com').css({'background-image':check_img[0]}).parent().siblings().find('.pay_check_com').css({'background-image':check_img[1]})
                 });
             };
         return {
