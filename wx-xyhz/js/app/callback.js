fb9acdf8dcb12e2c7b07244e477f32bf8333e120
qiuguixin <qguixin@qq.com>
2016-04-01 11:30:31 +0800

    搜索fixed

--- wx-xyhz/js/app/callback.js
+++ wx-xyhz/js/app/callback.js
@@ -3,7 +3,7 @@
  */
 //请求数据服务
 
-define(['jquery','cookieCrud', 'beforeSend', 'paging', 'config'],
+define(['jquery', 'cookieCrud', 'beforeSend', 'paging', 'config'],
     function($, cookieCrud, beforeSend, paging, config) {
         var
             showGoodsListData = function(li) {
@@ -16,23 +16,27 @@ define(['jquery','cookieCrud', 'beforeSend', 'paging', 'config'],
                     } else if (li.data.length < config.pageSize) {
                         $('#loader').hide();
                         config.loadFlag = false;
-                    }else{
+                    } else {
                         var nextpagehtml = '';
-                    for (var i = 0; i < li.data.length; i++) {
-                        var price = parseFloat(li.data[i].p_price).toFixed(2);
-                        nextpagehtml += '<li class="item" data-goodsid="' + li.data[i].goods_id + '">';
-                        nextpagehtml += '<div class="img-c" style="background-image: url(' + li.data[i].image_url + ')">';
-                        nextpagehtml += li.data[i].logo_image == '' ? '' : '<img class="bq-icon" src="' + li.data[i].logo_image + '" alt="">';
-                        nextpagehtml += li.data[i].store == '0' ? '<span class="guang">卖光了</span>' : '';
-                        nextpagehtml += '</div>';
-                        nextpagehtml += '<div class="info-c">';
-                        nextpagehtml += '<h3>' + li.data[i].short_name + '</h3>';
-                        nextpagehtml += '<span class="price">¥<strong>' + price + '</strong></span>';
-                        nextpagehtml += '<span class="love">' + li.data[i].like_count + '</span>';
-                        nextpagehtml += '</div></li>';
-                    }
-                    $('.ui-fall').html($('.ui-fall').html() + nextpagehtml);
-                    config.loadFlag = true;
+                        for (var i = 0; i < li.data.length; i++) {
+                            var price = parseFloat(li.data[i].p_price).toFixed(2);
+                            nextpagehtml += '<li class="item" data-goodsid="' + li.data[i].goods_id + '">';
+                            nextpagehtml += '<div class="img-c" style="background-image: url(' + li.data[i].image_url + ')">';
+                            nextpagehtml += li.data[i].logo_image == '' ? '' : '<img class="bq-icon" src="' + li.data[i].logo_image + '" alt="">';
+                            nextpagehtml += li.data[i].store == '0' ? '<span class="guang">卖光了</span>' : '';
+                            nextpagehtml += '</div>';
+                            nextpagehtml += '<div class="info-c">';
+                            nextpagehtml += '<h3>' + li.data[i].short_name + '</h3>';
+                            nextpagehtml += '<span class="price">¥<strong>' + price + '</strong></span>';
+                            nextpagehtml += '<span class="love">' + li.data[i].like_count + '</span>';
+                            nextpagehtml += '</div></li>';
+                        }
+                        $('.ui-fall').html($('.ui-fall').html() + nextpagehtml);
+                        $('.ui-fall li').on('click', function(event) {
+                            var goodsid = $(this).attr('data-goodsid');
+                            location.href = 'http://app.xinyihezi.com:8881/index.html#/goods/' + goodsid;
+                        });
+                        config.loadFlag = true;
                     }
                 } else {
                     $('.no-data').show();
@@ -41,49 +45,50 @@ define(['jquery','cookieCrud', 'beforeSend', 'paging', 'config'],
                     config.loadFlag = false;
                 }
             },
-            showKeyListData = function (ret) {
-                    if (ret.status == '0') {
-                        config.label_count = ret.data.label_count;
-                        var label = ret.data.label;
-                        if (label == null || label.length == 0) {
-                            $('.hs-area').hide();
-                            $('#statusTip').html('没有了').fadeIn(500).delay(1000).fadeOut(500);
-                        } else {
-                            var len = label.length < 10 ? label.length : 10;
-                            var nexthtml = '';
-                            for (var i = 0; i < len; i++) {
-                                nexthtml += '<li><span class="txt">' + label[i].hot_word + '</span>';
-                                nexthtml += label[i].label_info == '' ? '' : '<span class="tag" style="background-color:' + label[i].label_color + '">' + label[i].label_info + '</span></li>'
-                            }
-                            $('#loadImg').hide();
-                            $('.hs-area ul').html(nexthtml).show();
-                        }
-                        $('.hs-area ul li').on('click', function(event) {
-                            var key = $(this).find('.txt').text();
-                            $('.hs-area').hide();
-                            $('.history-area').hide();
-                            $('.search-ret-area').show();
-                            cookieCrud.addCoo(key);
-                            doSearch(key);
-                        });
-                        //点选历史记录
-                        $('.history-area ul').on('click', 'li', function(event) {
-                            $('.history-area').hide();
-                            $('.history-area').hide();
-                            $('.search-ret-area').show();
-                            var key = $(this).text();
-                            doSearch(key);
-                        });
-                    } else {
+            showKeyListData = function(ret) {
+                if (ret.status == '0') {
+                    config.label_count = ret.data.label_count;
+                    var label = ret.data.label;
+                    if (label == null || label.length == 0) {
                         $('.hs-area').hide();
                         $('#statusTip').html('没有了').fadeIn(500).delay(1000).fadeOut(500);
+                    } else {
+                        var len = label.length < 10 ? label.length : 10;
+                        var nexthtml = '';
+                        for (var i = 0; i < len; i++) {
+                            nexthtml += '<li><span class="txt">' + label[i].hot_word + '</span>';
+                            nexthtml += label[i].label_info == '' ? '' : '<span class="tag" style="background-color:' + label[i].label_color + '">' + label[i].label_info + '</span></li>'
+                        }
+                        $('#loadImg').hide();
+                        $('.hs-area ul').html(nexthtml).show();
                     }
-                    function doSearch(key) {
-                        var url = encodeURI('good/?search_type=1&name=' + key);
-                        console.log(key);
-                        paging.pageGetData(url, beforeSend.showGoodsListDataLoading, showGoodsListData);
-                    }
-                };
+                    $('.hs-area ul li').on('click', function(event) {
+                        var key = $(this).find('.txt').text();
+                        $('.hs-area').hide();
+                        $('.history-area').hide();
+                        $('.search-ret-area').show();
+                        cookieCrud.addCoo(key);
+                        doSearch(key);
+                    });
+                    //点选历史记录
+                    $('.history-area ul').on('click', 'li', function(event) {
+                        $('.history-area').hide();
+                        $('.history-area').hide();
+                        $('.search-ret-area').show();
+                        var key = $(this).text();
+                        doSearch(key);
+                    });
+                } else {
+                    $('.hs-area').hide();
+                    $('#statusTip').html('没有了').fadeIn(500).delay(1000).fadeOut(500);
+                }
+
+                function doSearch(key) {
+                    var url = encodeURI('good/?search_type=1&name=' + key);
+                    console.log(key);
+                    paging.pageGetData(url, beforeSend.showGoodsListDataLoading, showGoodsListData);
+                }
+            };
 
         return {
             showGoodsListData: showGoodsListData,
