2be98a4a5df1c2b63af11768144953408b4d604f
shistriver <954917356@qq.com>
2016-04-05 10:07:40 +0800

    限制字符优化

--- wx-xyhz/js/app/limitChar.js
+++ wx-xyhz/js/app/limitChar.js
@@ -6,31 +6,14 @@
 define(['jquery'],function($) {
     var limit = function(id,booleanV,className,max){
         //限制输入框字数
-/*
-        $(id).bind('input propertychange', function() {
-            var oValue = $(this).val();
-            var num = Math.ceil(getLength($(id).val())/2);
-            console.log(num);
-            if(oValue=='' || num > max){
-                oValue = oValue.substr(0,max);
-                $(this).val(oValue);
-                if(booleanV){
-                    $(className).html(max);
-                }
-                return;
-            }else{
-                if(booleanV){
-                    $(className).html(num);
-                }
-            }
-        });
-*/
-
         $(id).bind('input propertychange', function() {
             var curLength = $(this).val().length;
             if(curLength > max){
                 var oValue = $(this).val().substr(0,max);
                 $(this).val(oValue);
+                if(booleanV){
+                    $(className).html(max);
+                }
             }else{
                 if(booleanV){
                     $(className).text(curLength);
